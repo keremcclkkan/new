7ad2dbad117027743e08828066b1c4327f773def
keremcclkkan <92017550+keremcclkkan@users.noreply.github.com>
2022-03-30 14:45:24 +0300

    main

---
+++ appConfig.js
@@ -0,0 +1,7 @@
+connectionString = "postgres://cxnfofuwrejsfk:5eee3d06359ff7b85a152a2668fbd2387d5d1e3e1445d8ccd006f5a248befa26@ec2-52-18-116-67.eu-west-1.compute.amazonaws.com:5432/dee6t5kup87ql7";
+
+module.exports = {
+    connectionString: connectionString
+
+}
+    
